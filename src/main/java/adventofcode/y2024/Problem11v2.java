d9f4a951fb7f698c9ad9b91c964fa0e08c18a17f
dashie <dashie.dev@gmail.com>
2024-12-11 11:41:47 +0100

    puzzle: alternative solution based on bag for Day 11

---
+++ src/main/java/adventofcode/y2024/Problem11v2.java
@@ -0,0 +1,90 @@
+package adventofcode.y2024;
+
+import adventofcode.commons.AoCInput;
+import adventofcode.commons.AoCProblem;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.lang.Long.parseLong;
+
+/**
+ * Day 11: Plutonian Pebbles
+ * https://adventofcode.com/2024/day/11
+ */
+public class Problem11v2 extends AoCProblem<Long> {
+
+    public static void main(String[] args) throws Exception {
+        new Problem11v2().solve(false);
+    }
+
+    private List<Long> data0;
+
+    @Override
+    public void processInput(AoCInput input) throws Exception {
+        data0 = input.toListOfLongList(" ").getFirst();
+    }
+
+    /**
+     * ...Consider the arrangement of stones in front of you.
+     * How many stones will you have after blinking 25 times?
+     */
+    @Override
+    public Long partOne() throws Exception {
+        return count(25);
+    }
+
+    private long count(int round) {
+        Map<Long, Long> bag = toBag(data0);
+        while (round > 0) {
+            Map<Long, Long> bagAfterBlink = new HashMap<>();
+            bag.entrySet().forEach(e -> {
+                long[] newStones = blink(e.getKey());
+                long n = e.getValue();
+                bagAfterBlink.compute(newStones[0], (k, v) -> v == null ? n : v + n);
+                if (newStones.length > 1) bagAfterBlink.compute(newStones[1], (k, v) -> v == null ? n : v + n);
+            });
+            bag = bagAfterBlink;
+            round--;
+        }
+
+        return bag.values().stream().mapToLong(n -> n).sum();
+    }
+
+    public Map<Long, Long> toBag(List<Long> data) {
+        return data.stream().collect(Collectors.groupingBy(n -> n, Collectors.counting()));
+    }
+
+    /**
+     * - If the stone is engraved with the number 0,
+     *   it is replaced by a stone engraved with the number 1.
+     * - If the stone is engraved with a number that has an even number of digits,
+     *   it is replaced by two stones. The left half of the digits are engraved on the
+     *   new left stone, and the right half of the digits are engraved on the new right stone.
+     *   (The new numbers don't keep extra leading zeroes: 1000 would become stones 10 and 0.)
+     * - If none of the other rules apply, the stone is replaced by a new stone;
+     *   the old stone's number multiplied by 2024 is engraved on the new stone.
+     */
+    private long[] blink(long n) {
+        if (n == 0) {
+            return new long[]{1L};
+        } else if (Long.toString(n).length() % 2 == 0) {
+            String ns = Long.toString(n);
+            long n1 = parseLong(ns.substring(0, ns.length() / 2));
+            long n2 = parseLong(ns.substring(ns.length() / 2));
+            return new long[]{n1, n2};
+        } else {
+            return new long[]{n * 2024L};
+        }
+    }
+
+    /**
+     * ...How many stones would you have after blinking a total of 75 times?
+     */
+    @Override
+    public Long partTwo() throws Exception {
+        return count(75);
+    }
+}
---
+++ src/test/java/adventofcode/y2024/Problem11Test.java
@@ -0,0 +1,15 @@
+package adventofcode.y2024;
+
+import org.junit.jupiter.api.Test;
+
+public class Problem11Test extends AbstractProblemTest {
+
+    public Problem11Test() {
+        super(Problem11.class);
+    }
+
+    @Test
+    public void testProblem() throws Exception {
+        _testProblem(220722, 261952051690787L);
+    }
+}
